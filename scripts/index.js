da106df1e73c6c1f2bb2cb809b7377a7f16197af
Nikita Malenkiy <ma.ready.to.blow@gmail.com>
2023-03-03 19:05:22 +0600

    fix/img

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,22 @@ let profileName = document.querySelector('.profile__name');
 let profileEditButton = document.querySelector('.profile__edit-button');
 let profileDescription = document.querySelector('.profile__description');
 let popUp = document.querySelector('.popup');
-let popUpForm = popUpEdit.querySelector('.popup__form');
-let popUpName = popUpEdit.querySelector('.popup__input_field_name');
-let popUpDescription = popUpEdit.querySelector('.popup__input_field_description');
-let popUpCloseButton = popUpEdit.querySelector('.popup__close-button');
+let popUpForm = popUp.querySelector('.popup__form');
+let popUpName = popUp.querySelector('.popup__input_field_name');
+let popUpDescription = popUp.querySelector('.popup__input_field_description');
+let popUpCloseButton = popUp.querySelector('.popup__close-button');
 
 // Открытие попапа с исходными данными пользователя
-function popUpOpening() {}
+function popUpOpening() {
+  popUpName.value = profileName.textContent;
+  popUpDescription.value = profileDescription.textContent;
+  popUp.classList.add('popup_opened');
+}
 
-// function popUpOpening() {
-//   popUpName.value = profileName.textContent;
-//   popUpDescription.value = profileDescription.textContent;
-//   popUp.classList.add('popup_opened');
-// }
-
-// // Закрытие попапа
-// function popUpClose() {
-//   popUp.classList.remove('popup_opened');
-// }
+// Закрытие попапа
+function popUpClose() {
+  popUp.classList.remove('popup_opened');
+}
 
 // Заполнение полей данными пользователя
 function handleFormSubmit(evt) {
